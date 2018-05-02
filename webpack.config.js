40d6377ecf722b05e8c639dc9fc4f8d0d85cfc2d
Christos Ziogas <ziogasc@gmail.com>
2018-05-02 23:47:10 +0300

    multiple entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+
+const path = require('path');
+
 const config = {
-    entry: './templates/synathina/js/app/app.js'
+    entry: {
+        app: './templates/synathina/js/app/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    }
 };
 
 module.exports = config;
