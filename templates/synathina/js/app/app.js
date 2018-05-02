b0d1506eb56b245455f1e276f0467c39c9776e28
Christos Ziogas <ziogasc@gmail.com>
2018-05-02 23:31:26 +0300

    ignore line break for now

--- templates/synathina/js/app/app.js
+++ templates/synathina/js/app/app.js
@@ -1,8 +1,8 @@
 window.EVT = new EventEmitter2();
 
 var App = (function(global){
-	// init App on window load
-	$(global).load(function(){
-		EVT.emit('init');
-	});
+    // init App on window load
+    $(global).load(function(){
+        EVT.emit('init');
+    });
 })(window);
