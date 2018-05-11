1a3780214d618aaa463b062f1c4f372bc06a1f98
Dennis Dasios <ddasios@steficon.gr>
2018-05-11 17:44:58 +0300

    fixes

--- templates/synathina/html/com_content/article/default.php
+++ templates/synathina/html/com_content/article/default.php
@@ -2,20 +2,21 @@
 defined('_JEXEC') or die;
 
 
-function youtubeID($url){
- $res = explode("v=",$url);
- if(isset($res[1])) {
-	$res1 = explode('&',$res[1]);
-	if(isset($res1[1])){
+function youtubeID($url)
+{
+ $res = explode('v=', $url);
+ if (isset($res[1])) {
+	$res1 = explode('&', $res[1]);
+	if (isset($res1[1])) {
 		$res[1] = $res1[0];
 	}
 	$res1 = explode('#',$res[1]);
-	if(isset($res1[1])){
-		$res[1] = $res1[0];
+	if (isset($res1[1])) {
+		$res[1]  = $res1[0];
 	}
  }
- return substr($res[1],0,12);
-	 return false;
+
+ return substr($res[1], 0, 12);
 }
 
 //db connection
